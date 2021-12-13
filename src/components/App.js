967fa21ec16ec8e8247bd43dd20b83e08ba15e75
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-12 20:49:56 -0500

    basic routing established

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import styled from 'styled-components';
 
 import Header from './Header';
 import BloomHeader from './BloomHeader';
 import Login from './Login';
+import View from './View';
+import Logout from './Logout';
 
 const App = () => {
   return (
@@ -12,9 +14,20 @@ const App = () => {
       <BloomHeader/>
       <Header/>
       <RouteContainer>
-        <Route exact path="/">
-          <Login/>
-        </Route>          
+        <Switch>
+          <Route path='/logout'>
+            <Logout />
+          </Route>
+          <Route path='/view'>
+            <View />
+          </Route>
+          <Route path='/login'>
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Login/>
+          </Route>       
+        </Switch>
       </RouteContainer>
     </AppContainer>
   )
