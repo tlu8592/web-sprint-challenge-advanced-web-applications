7c6fc20aebc6f135d2943b393c4816834479383b
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-12-12 21:30:33 -0500

    completed logic for PrivateRoute

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 
-function PrivateRoute() {
-  return (<Route/>);
+function PrivateRoute({component: Component, ...rest}) {
+  return (<Route {...rest} render={
+    (props) => {
+      if (localStorage.getItem("token")) {
+        return <Component {...props} />
+      } else {
+        return(<Redirect to='/' />)
+      }
+    }
+  }/>);
 }
 
 export default PrivateRoute;
